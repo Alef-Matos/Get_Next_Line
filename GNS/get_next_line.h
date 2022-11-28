8bc91a25d9f5604ec9ec099cdc6d04393d644536
almatos <almatos@student.42.fr>
2022-11-28 18:43:00 +0000

    TESTE

--- GNS/get_next_line.h
+++ GNS/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 09:27:10 by almatos           #+#    #+#             */
-/*   Updated: 2022/11/28 16:50:07 by almatos          ###   ########.fr       */
+/*   Updated: 2022/11/28 18:00:43 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 45
 # endif
+
+typedef struct s_gnl
+{
+	char		*line;
+	long		index;
+	long		size_buff;
+	long		size_line;
+}	t_gnl;
 
 int			gns_utils_len(const char *str, int len);
 
