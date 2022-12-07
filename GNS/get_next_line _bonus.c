de7d0558ede309153c592b6a25bcc726dafdcd27
almatos <almatos@student.42.fr>
2022-12-07 16:49:58 +0000

    up

--- GNS/get_next_line _bonus.c
+++ GNS/get_next_line _bonus.c
@@ -1,39 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line _bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 09:27:07 by almatos           #+#    #+#             */
-/*   Updated: 2022/12/07 15:16:02 by almatos          ###   ########.fr       */
+/*   Updated: 2022/12/07 16:04:01 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(long fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
+	static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
 	char		*line;
 	long		size_read;
 
 	if (fd < 0 || fd > FOPEN_MAX || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = next_line(buffer, 0);
-	if (clean(buffer))
+	line = next_line(buffer[fd], 0);
+	if (clean(buffer[fd]))
 		return (line);
 	size_read = 1;
 	while (size_read > 0)
 	{
-		size_read = read(fd, buffer, BUFFER_SIZE);
+		size_read = read(fd, buffer[fd], BUFFER_SIZE);
 		if (size_read == -1 || (size_read == 0 && line[0] == '\0'))
 		{
 			free(line);
 			return (NULL);
 		}
-		line = next_line(buffer, line);
-		if (clean(buffer))
+		line = next_line(buffer[fd], line);
+		if (clean(buffer[fd]))
 			break ;
 	}
 	return (line);
--- GNS/get_next_line_bonus.h
+++ GNS/get_next_line_bonus.h
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 09:27:10 by almatos           #+#    #+#             */
-/*   Updated: 2022/12/07 15:19:54 by almatos          ###   ########.fr       */
+/*   Updated: 2022/12/07 15:58:28 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
 
-# define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include	<stdlib.h>
 # include	<unistd.h>
